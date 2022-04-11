c515194fcd174fab5128a3d8381db0c11a0e8543
Mojinet <jeanbaptiste.cochinard@gmail.com>
2022-04-11 17:50:27 +0200

    add asynchronous

---
+++ JS/asynchronous.js
@@ -0,0 +1,53 @@
+/***************************************************/
+// ** Asynchronous
+// all asynchronous call placed in "event loop", who is a call stack
+/***************************************************/
+
+// Time method
+// setTimeOut(function, time in ms)
+// clearTimeout() for cancel before execute
+// setInterval(function, time in ms) executed all Xms, use clearInterval() for stop loop
+// setImmediate(function) placed function in high of call stack
+
+// Callbacks : prefer use promise
+// The callback is a method who is call after un asynchronous execution like with setTimeout() for example :
+setTimeout(function() {
+    console.log("I'm here!") // extecuted after 5000ms
+}, 5000);
+
+// Is best to prevent error like :
+fs.readFile(filePath, function(err, data) {
+    if (err) {
+        throw err;
+    }
+    // Do something with data
+});
+
+// Promises
+// Promise is an object who is returned immediatly after call, but .then() is executed when is finish and .catch() in case of error
+returnAPromiseWithNumber2()
+    .then(function(data) { // Data is 2
+        return data + 1;
+    })
+    .then(function(data) { // Data is 3
+        throw new Error('error');
+    })
+    .then(function(data) {
+        // Not executed
+    })
+    .catch(function(err) {
+        return 5;
+    })
+    .then(function(data) { // Data is 5
+        // Do something
+    });
+
+// Async/await
+async function fonctionAsynchrone1() {/* code asynchronous */}
+async function fonctionAsynchrone2() {/* code asynchronous */}
+
+async function fonctionAsynchrone3() {
+    const value1 = await fonctionAsynchrone1();
+    const value2 = await fonctionAsynchrone2();
+    return value1 + value2;
+}
