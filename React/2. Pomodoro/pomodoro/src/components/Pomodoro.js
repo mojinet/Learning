1bdad2599fd994f46005f2f1a409b8890c63d103
Mojinet <jeanbaptiste.cochinard@gmail.com>
2022-04-29 12:38:05 +0200

    add sass

--- React/2. Pomodoro/pomodoro/src/components/Pomodoro.js
+++ React/2. Pomodoro/pomodoro/src/components/Pomodoro.js
@@ -1,9 +1,31 @@
-import '../style/Pomodoro.css'
+import '../style/Pomodoro.scss'
+import playIcon from '../assets/002-play-button.png'
+import stopIcon from '../assets/001-stop-button.png'
+import resetIcon from '../assets/003-reset.png'
 
 function Pomodoro(){
     return (
         <div className="Pomodoro">
-
+            <div className="top-progress"></div>
+            <div className="timers">
+                <div className="work-timer">
+                    00 : 45 : 00
+                </div>
+                <div className="pause-timer">
+                    00 : 5 : 00
+                </div>
+            </div>
+            <div className="controls">
+                <div className="play-control">
+                    <img src={playIcon} alt="Play icon"/>
+                </div>
+                <div className="stop-control">
+                    <img src={stopIcon} alt="Stop icon"/>
+                </div>
+                <div className="reset-control">
+                    <img src={resetIcon} alt="Reset icon"/>
+                </div>
+            </div>
         </div>
     )
 }
