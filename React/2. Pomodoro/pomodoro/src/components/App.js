cebaba457bf07ae72635a30e31b0ada996573067
Mojinet <jeanbaptiste.cochinard@gmail.com>
2022-04-29 12:26:04 +0200

    init pomodoro in react

---
+++ React/2. Pomodoro/pomodoro/src/components/App.js
@@ -0,0 +1,13 @@
+import logo from '../logo.svg';
+import '../style/App.css';
+import Pomodoro from "./Pomodoro";
+
+function App() {
+  return (
+    <div className="App">
+      <Pomodoro />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/2. Pomodoro/pomodoro/src/components/Pomodoro.js
@@ -0,0 +1,11 @@
+import '../style/Pomodoro.css'
+
+function Pomodoro(){
+    return (
+        <div className="Pomodoro">
+
+        </div>
+    )
+}
+
+export default Pomodoro
